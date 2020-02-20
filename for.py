1ca45d2363cc7813b9a349e1a20583cb3ed4697f
igni <igni@gurtam.com>
2020-02-20 18:27:57 +0300

    something

---
+++ for.py
@@ -0,0 +1,6 @@
+m = [1,24,52,-123,-23,-22,25,26]
+print('m: ', len(m))
+
+for j in range(len(m) - 1):
+	for i in range(len(m) -1):
+		print('i: ', i)
---
+++ test.py
@@ -0,0 +1,8 @@
+N = 5
+M = 3
+
+
+for m in range(M):
+	for n in range(N):
+		print('*', end="")
+	print(end="\r\n")
