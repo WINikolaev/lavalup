1708c40ce5297f7db4449f5366bb41b17c705aa3
igni <igni@gurtam.com>
2020-03-10 14:17:06 +0300

    test block speacing for game dev

--- cpp_high_perfomance/3chapter/main.cpp
+++ cpp_high_perfomance/3chapter/main.cpp
@@ -9,5 +9,22 @@ using namespace std;
 int main() {
 	vector<int>vec{1,2,3,4,5,6,7,8,9};
 	binary_search(vec, 4);
+
+	int Size = 5;
+
+	const auto numBlocks = Size * Size;
+	const auto BlockSpacing = 300.f;
+
+	for (size_t BlockIndex = 0; BlockIndex < numBlocks; BlockIndex++)
+	{
+		const float XOffset = (BlockIndex%Size) * BlockSpacing; // Divide by dimension
+		const float YOffset = (BlockIndex/Size) * BlockSpacing; // Modulo gives remainder
+		if (!XOffset && YOffset) { cout << endl;}
+		
+		cout << YOffset << "," << XOffset << "			";
+		//cout << "XOffset: " << XOffset << "; YOffset: " << YOffset << endl;
+		//cout << "####################################" << endl;
+	}
+	
 	return 0;
 }
